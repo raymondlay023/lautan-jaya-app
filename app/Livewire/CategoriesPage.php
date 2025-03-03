3f989bbded79892c7db3250d68590ff34b28846c
Raymond Lay <77760136+raymondlay023@users.noreply.github.com>
2025-03-03 15:09:15 +0700

    make home and categories page dynamic

--- app/Livewire/CategoriesPage.php
+++ app/Livewire/CategoriesPage.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Categories - Lautan Jaya App')]
 class CategoriesPage extends Component
 {
     public function render()
     {
-        return view('livewire.categories-page');
+        $categories = Category::all();
+        return view('livewire.categories-page', compact('categories'));
     }
 }
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>{{ $title ?? 'Lautan Jaya' }}</title>
+    <title>{{ $title ?? 'Lautan Jaya App' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
