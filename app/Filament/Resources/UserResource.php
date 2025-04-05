f9244811dfb6fa227cc4e71145552efa09adfec1
Raymond Lay <raymondlay023@gmail.com>
2025-04-05 11:16:27 +0700

    add order placed mail and checkout page dynamic

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -36,10 +36,10 @@ class UserResource extends Resource
                     ->unique(ignoreRecord: true)
                     ->maxLength(255),
                 Forms\Components\DateTimePicker::make('email_verified_at'),
-                Forms\Components\TextInput::make('password')
-                    ->password()
-                    ->required()
-                    ->maxLength(255),
+                // Forms\Components\TextInput::make('password')
+                //     ->password()
+                //     ->required()
+                //     ->maxLength(255),
             ]);
     }
 
